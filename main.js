e898141f638af3846a3da5c1ac1b19517f5f5bf0
kien19quang <kien19quang@gmail.com>
2022-09-22 21:19:25 +0700

    done

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
 let isShow = false;
 
 let btnMenu = document.querySelector(".icon-menu");
-console.log(btnMenu);
+let shadowContainer = document.querySelector(".shadow-container");
+let sideBar = document.querySelector(".side-bar");
+
+btnMenu.onclick = () => {
+  sideBar.style.display = "block";
+  shadowContainer.style.display = "block";
+};
+
+shadowContainer.onclick = () => {
+  sideBar.style.display = "none";
+  shadowContainer.style.display = "none";
+};
